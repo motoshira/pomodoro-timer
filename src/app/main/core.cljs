d6b2b8d60afac6be3365489d837a8c54b7524cde
motoshira <51407064+motoshira@users.noreply.github.com>
2021-06-02 01:11:56 +0900

    Add changes

--- src/app/main/core.cljs
+++ src/app/main/core.cljs
@@ -5,10 +5,13 @@
 
 (defn init-browser []
   (reset! main-window (BrowserWindow.
-                        (clj->js {:width 800
-                                  :height 600
-                                  :webPreferences
-                                  {:nodeIntegration true}})))
+                       (clj->js {:width 300
+                                 :height 500
+                                 :transparent false
+                                 :frame true
+                                 :always-on-top true
+                                 :webPreferences
+                                 {:nodeIntegration true}})))
   ; Path is relative to the compiled js file (main.js in our case)
   (.loadURL ^js/electron.BrowserWindow @main-window (str "file://" js/__dirname "/public/index.html"))
   (.on ^js/electron.BrowserWindow @main-window "closed" #(reset! main-window nil)))
--- src/app/renderer/core.cljs
+++ src/app/renderer/core.cljs
@@ -1,20 +1,108 @@
 (ns app.renderer.core
-  (:require [reagent.core :refer [atom]]
-            [reagent.dom :as rd]))
+  (:require [reagent.core :as r]
+            [reagent.ratom :as ratom]
+            [reagent.dom :as rd]
+            [goog.string :as gstr]
+            [helins.timer :as timer]))
 
 (enable-console-print!)
 
-(defonce state (atom 0))
+;;;
+;;; db
+;;;
+
+(def ^:private init-time (* 60 30))
+
+(defonce ^:private db
+  (r/atom {:timer nil
+           :time init-time}))
+
+;;;
+;;; events
+;;;
+
+(defn- dec-time! []
+    (swap! db update-in [:time] #(max 0 (- % 1))))
+
+(defn- set-interval! []
+  (js/setInterval
+   dec-time!
+   1000))
+
+(defn- clear-interval! [this]
+  (js/clearInterval this))
+
+(defn- toggle-timer! []
+  ;; TODO: 間接的にstart/stopしたい
+  (if-not (get @db :timer)
+    (do
+      (swap! db
+             assoc :timer
+             (set-interval!)))
+    (do
+      (clear-interval! (:timer @db))
+      (swap! db assoc :timer nil))))
+
+(defn- reset-timer! []
+  (when-let [timer (get @db :timer)]
+    (clear-interval! timer))
+  (swap! db assoc :timer nil)
+  (swap! db assoc :time init-time))
+
+
+;;;
+;;; subs
+;;;
+
+(defn- sec->min-sec [seconds]
+  {:minutes (quot seconds 60)
+   :seconds (rem seconds 60)})
+
+;;;
+;;; views
+;;;
+
+(def ^:private styles
+  (let [basic-attrs {:height "520px"
+                     :width "320px"
+                     :overflow :visible
+                     :display :flex
+                     :flex-direction :column
+                     :justify-content :center
+                     :align-items :center
+                     :background "#000"
+                     :color "#FFF"
+                     "&::-webkit-scrollbar" {:display :none}}]
+    {:root-div basic-attrs
+     :root-div-transparent (assoc basic-attrs
+                                  :background "rgba(0, 0, 0, 0.9)")
+     :message {:flex-glow 1}
+     :buttons {:display :flex
+               :flex-glow 1
+               :padding-top "20px"}
+     :button {:background "#000"
+              :color "#FFF"}}))
 
 (defn root-component []
-  [:div
-   [:div.logos
-    [:img.electron {:src "img/electron-logo.png"}]
-    [:img.cljs {:src "img/cljs-logo.svg"}]
-    [:img.reagent {:src "img/reagent-logo.png"}]]
-   [:button
-    {:on-click #(swap! state inc)}
-    (str "Clicked " @state " times")]])
+  (let [cur-time (get-in @db [:time])
+        timer (get-in @db [:timer])
+        time-up? (zero? cur-time)
+        root-style (get styles
+                        (if (or time-up?
+                                (nil? timer))
+                          :root-div
+                          :root-div-transparent))
+        {:keys [:minutes :seconds]} (sec->min-sec cur-time)]
+    [:div {:style (:root-div styles)}
+     (gstr/format "Time is now: %02d:%02d" minutes seconds)
+     [:br]
+     [:div {:style (:buttons styles)}
+      (for [[on-click label] [[#(toggle-timer!) "Start/Stop"]
+                              [#(reset-timer!) "Reset"]]]
+        [:button {:key label
+                  :on-click on-click
+                  :style (:button styles)}
+         label])]]))
 
 (defn ^:dev/after-load start! []
   (rd/render
