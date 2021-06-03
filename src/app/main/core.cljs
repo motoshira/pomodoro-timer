fd80315948236e2762739bcaf39b07b368d3bb3f
motoshira <51407064+motoshira@users.noreply.github.com>
2021-06-03 13:29:00 +0900

    Disable shadow

--- src/app/main/core.cljs
+++ src/app/main/core.cljs
@@ -13,6 +13,7 @@
                                  :webPreferences
                                  {:nodeIntegration true}})))
   (.setAlwaysOnTop @main-window true "floating")
+  (.setHasShadow @main-window false)
   ; Path is relative to the compiled js file (main.js in our case)
   (.loadURL ^js/electron.BrowserWindow @main-window (str "file://" js/__dirname "/public/index.html"))
   (.on ^js/electron.BrowserWindow @main-window "closed" #(reset! main-window nil)))
